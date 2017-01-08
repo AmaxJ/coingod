a57b68a69a34fa63106789f29ccfc26db6686bd3
alan johnson <ajohnson88@gmail.com>
2017-01-07 22:43:45 -0500

    basic bitcoin price messaging

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const SlackBot = require('slackbots');
 const _ = require('lodash');
+const axios = require('axios');
 
 // create a bot
 const bot = new SlackBot({
@@ -7,7 +8,26 @@ const bot = new SlackBot({
     name: process.env.SLACKBOT_NAME
 });
 
-bot.on('start', () => {
+const config = {
+  as_user: true
+}
+
+bot.on('start', _bootstrap);
+
+bot.on('message', (message) => {
+    if (message.type !== 'message' || !message.text || !_botCalled(message.text)) {
+      return;
+    }
+    const channel = _getChannel(message.channel);
+
+    _getBitcoinPrice()
+      .then((bitcoinData) => {
+        _postMessage(bitcoinData, channel);
+      })
+      .catch(console.err);
+})
+
+function _bootstrap() {
   bot.getChannels()
     .then((response) => {
       bot._channels = response.channels;
@@ -19,23 +39,38 @@ bot.on('start', () => {
       bot._groups = response.groups;
     })
     .catch(console.err);
-    // bot.user = _.filter(bot.users, (_user) => {
-    //   return _user.name === bot.name;
-    // })[0];
-});
+}
 
-bot.on('message', (message) => {
+function _postMessage(bitcoinData, channelData) {
+  const { price, market } = bitcoinData;
+  const formattedPrice = _.parseInt(price).toFixed(2);
+  const { channel_type, channel } = channelData;
+  const message = `The bitcoin price is currently $${formattedPrice} on ${market}`;
 
+  if (channel_type === 'public') {
+    bot.postMessageToChannel(channel, message, config);
+  } else {
+    bot.postMessageToGroup(channel, message, config);
+  }
+}
 
-    if (message.type !== 'message' || !message.text || !_botCalled(message.text)) {
-      console.log('no params met');
-      return;
-    }
-    const channel = _getChannelName(message.channel);
+function _getBitcoinPrice(exchange='bitstamp') {
+  return axios.get('https://api.cryptonator.com/api/full/btc-usd')
+    .then((response) => {
+      const formattedExchange = exchange.toLowerCase();
+      const _exchange = _.find(response.data.ticker.markets, (marketData) => {
+        return marketData.market.toLowerCase() === formattedExchange;
+      });
+      if (_exchange) {
+        return _exchange;
+      }
+      return {
+        market: 'average',
+        price: response.data.ticker.price
+      }
+    });
+}
 
-    bot.postMessageToGroup('testing', `your\'re in ${channel}` , {as_user: true});
-
-})
 
 function _isValidMessage(message) {
   return message.type === 'message' && !!message.text
@@ -45,14 +80,16 @@ function _botCalled(text) {
   return _.lowerCase(text).indexOf('coingod') > -1;
 }
 
-function _getChannelName(id) {
-
+function _getChannel(id) {
   const channel = _.find(bot._channels, (channel) => {
     return channel.id === id;
   });
 
   if (channel){
-    return channel.name;
+    return {
+      channel_type: 'public',
+      channel: channel.name
+    }
   }
 
   const group = _.find(bot._groups, (group) => {
@@ -60,6 +97,10 @@ function _getChannelName(id) {
   });
 
   if (group) {
-    return group.name;
+    return {
+      channel_type: 'private',
+      channel: group.name
+    }
   }
+
 }
