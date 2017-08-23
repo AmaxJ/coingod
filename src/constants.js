a886c775cdcdf5bf151c46f033b4cc6eaadccccd
Alan <ajohnson88@gmail.com>
2017-08-23 00:15:15 -0400

    add coins

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
 module.exports = {
     GDAX_ETH_ENDPOINT: 'https://api.gdax.com/products/ETH-USD/stats',
     GDAX_BTC_ENDPOINT: 'https://api.gdax.com/products/BTC-USD/stats',
-    COINS: ['ripple', 'litecoin', 'monero', 'augur', 'golem', 'zcash', 'iconomi', 'melon', 'edgeless', 'blackcoin']
+    COINS: ['ripple', 'litecoin', 'monero', 'augur', 'golem', 'zcash',
+        'iconomi', 'melon', 'edgeless', 'blackcoin', '0x', 'district0x',
+        'status', 'bitcoin-cash', 'basic-attention-token', 'iota', 'neo',
+        'monero', 'omisego', 'civic', 'digibyte', 'storj']
 };
