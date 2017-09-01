111493c2cd918c0b20511d562d3b3aee7188b57f
Alan <ajohnson88@gmail.com>
2017-09-01 01:40:49 -0400

    sort coins by length

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,29 @@
+const _ = require('lodash');
+
 module.exports = {
     GDAX_ETH_ENDPOINT: 'https://api.gdax.com/products/ETH-USD/stats',
     GDAX_BTC_ENDPOINT: 'https://api.gdax.com/products/BTC-USD/stats',
-    COINS: ['ripple', 'litecoin', 'monero', 'augur', 'golem', 'zcash',
-        'iconomi', 'melon', 'edgeless', 'blackcoin', '0x', 'district0x',
-        'status', 'bitcoin-cash', 'basic-attention-token', 'iota', 'neo',
-        'monero', 'omisego', 'civic', 'digibyte', 'storj']
+    COINS: _.orderBy([
+        'ripple',
+        'litecoin',
+        'monero',
+        'augur',
+        'golem',
+        'zcash',
+        'iconomi',
+        'melon',
+        'edgeless',
+        'blackcoin',
+        '0x',
+        'district0x',
+        'status',
+        'bitcoin-cash',
+        'basic-attention-token',
+        'iota',
+        'neo',
+        'monero',
+        'omisego',
+        'civic',
+        'digibyte',
+        'storj'], ['length'], ['desc'])
 };
